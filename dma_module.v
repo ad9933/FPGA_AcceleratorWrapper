91df895395e8e5f9cf51425424aa22e05991534e
Honey(Desktop) <honeyjung@uos.ac.kr>
2021-07-30 02:09:42 +0900

    Finished DMA module

--- dma_module.v
+++ dma_module.v
@@ -8,77 +8,100 @@ module dma_module #(
 	//Burst per transaction
 	parameter BURST_SIZE = 16
 	
-	
+	//Number of burst needed
+	parameter BURST_NUM = DATA_SIZE / BURST_SIZE;
 	
 	
 )
 (
-	
+	///////////////////////////
+	//Control signals
+	///////////////////////////
+
+	//Read channel control
+	input					read_active,
+	input		[31:0]		read_address,
+
+	output	reg				read_idle,
+
+
+	//Write channel control
+	input					write_active,
+	input		[31:0]		write_address,
+
+	output 	reg 			write_idle,
+
+
+	//Read, write response data - {read response, write response}
+	output		[3:0]			rw_resp,
+
+
+
 	///////////////////////////
 	//AXI 3 Master interface
 	///////////////////////////
 	
 	//global signal
-	input			m_axi_acp_aclk,
-	input			axi_resetn,
+	input					m_axi_acp_aclk,
+	input					axi_resetn,
 	
 	
 	//read address channel
-	output	[2:0]	m_axi_acp_arid,		//OK
-	output	[31:0]	m_axi_acp_araddr,	//TODO : Zynq ignores last 2 bits(assumes that data is aligned every time)
-	output	[3:0]	m_axi_acp_arlen,	//OK
-	output	[2:0]	m_axi_acp_arsize,	//OK
-	output	[1:0]	m_axi_acp_arburst,	//OK
-	output	[1:0]	m_axi_acp_arlock,	//OK
-	output 	[3:0]	m_axi_acp_arcache,	//OK?
-	output	[2:0]	m_axi_acp_arprot,	//OK
-	output	[3:0]	m_axi_acp_arqos,	//OK
-	output	[4:0]	m_axi_acp_aruser,	//OK
-	output	reg		m_axi_acp_arvalid,	//OK
-	input			m_axi_acp_arready,	//OK
+	output		[2:0]		m_axi_acp_arid,		//OK
+	output	reg	[31:0]		m_axi_acp_araddr,	//TODO : Zynq ignores last 2 bits(assumes that data is aligned every time)
+	output		[3:0]		m_axi_acp_arlen,	//OK
+	output		[2:0]		m_axi_acp_arsize,	//OK
+	output		[1:0]		m_axi_acp_arburst,	//OK
+	output		[1:0]		m_axi_acp_arlock,	//OK
+	output 		[3:0]		m_axi_acp_arcache,	//OK?
+	output		[2:0]		m_axi_acp_arprot,	//OK
+	output		[3:0]		m_axi_acp_arqos,	//OK
+	output		[4:0]		m_axi_acp_aruser,	//OK
+	output	reg				m_axi_acp_arvalid,	//OK
+	input					m_axi_acp_arready,	//OK
 	
 	
 	//read data channel
-	input	[2:0]	m_axi_acp_rid,
-	input	[63:0]	m_axi_acp_rdata,
-	input	[1:0]	m_axi_acp_rresp,
-	input			m_axi_acp_rlast,
-	//input			m_axi_acp_ruser,
-	input			m_axi_acp_rvalid,
-	output			m_axi_acp_rready,
+	input		[2:0]		m_axi_acp_rid,
+	input		[63:0]		m_axi_acp_rdata,
+	input		[1:0]		m_axi_acp_rresp,
+	input					m_axi_acp_rlast,
+	//input					m_axi_acp_ruser,
+	input					m_axi_acp_rvalid,
+	output					m_axi_acp_rready,
 	
 	
 	//write address channel
-	output	[2:0]	m_axi_acp_awid,
-	output	[31:0]	m_axi_acp_awaddr,
-	output	[3:0]	m_axi_acp_awlen,
-	output	[2:0]	m_axi_acp_awsize,
-	output	[1:0]	m_axi_acp_awburst,
-	output	[1:0]	m_axi_acp_awlock,	
-	output	[3:0]	m_axi_acp_awcache,
-	output	[2:0]	m_axi_acp_awprot,
-	output	[3:0]	m_axi_acp_awqos,
-	output	[4:0]	m_axi_acp_awuser,
-	output			m_axi_acp_awvalid,
-	input			m_axi_acp_awready,
+	output		[2:0]		m_axi_acp_awid,
+	output	reg	[31:0]		m_axi_acp_awaddr,
+	output		[3:0]		m_axi_acp_awlen,
+	output		[2:0]		m_axi_acp_awsize,
+	output		[1:0]		m_axi_acp_awburst,
+	output		[1:0]		m_axi_acp_awlock,	
+	output		[3:0]		m_axi_acp_awcache,
+	output		[2:0]		m_axi_acp_awprot,
+	output		[3:0]		m_axi_acp_awqos,
+	output		[4:0]		m_axi_acp_awuser,
+	output	reg				m_axi_acp_awvalid,
+	input					m_axi_acp_awready,
 	
 	
 	//write data channel
-	output	[2:0]	m_axi_acp_wid,
-	output	[63:0]	m_axi_acp_wdata,
-	output	[7:0]	m_axi_acp_wstrb,
-	output			m_axi_acp_wlast,
-	output	[4:0]	m_axi_acp_wuser,
-	output			m_axi_acp_wvalid,
-	input			m_axi_acp_wready,
+	output		[2:0]		m_axi_acp_wid,
+	output		[63:0]		m_axi_acp_wdata,
+	output		[7:0]		m_axi_acp_wstrb,
+	output					m_axi_acp_wlast,
+	output		[4:0]		m_axi_acp_wuser,
+	output					m_axi_acp_wvalid,
+	input					m_axi_acp_wready,
 	
 	
 	//write response
-	input	[2:0]		m_axi_acp_bid,
-	input	[1:0]		m_axi_acp_bresp,
-	input	[4:0]		m_axi_acp_buser,
-	input				m_axi_acp_bvalid,
-	output				m_axi_acp_bready,
+	input		[2:0]		m_axi_acp_bid,
+	input		[1:0]		m_axi_acp_bresp,
+	input		[4:0]		m_axi_acp_buser,
+	input					m_axi_acp_bvalid,
+	output	reg				m_axi_acp_bready,
 	
 	
 	
@@ -88,11 +111,17 @@ module dma_module #(
 	//Stream Interface (Output)
 	////////////////////////////////////
 	
-	output	[63:0]		mm2s_data,
+	output	reg	[63:0]	mm2s_data,
 	output				mm2s_valid,
-	input				mm2s_ready
+	input				mm2s_ready,
 	
+	//////////////////////////////////// 
+	//Stream Interface (Input)
+	////////////////////////////////////
 	
+	input	[63:0]		s2mm_data,
+	input				s2mm_valid,
+	output				s2mm_ready
 	
 );
 
@@ -136,71 +165,177 @@ module dma_module #(
 	assign m_axi_acp_aruser = 5'bxxxxx;
 	assign m_axi_acp_awuser = 5'bxxxxx;
 	
-	
-	////////////////////////
+	//Response concat
+	assign rw_resp = {m_axi_acp_rresp, bresp};
+
+	////////////////////////////////////
 	//Register assignment
-	////////////////////////
+	////////////////////////////////////
 	
-	//DMA target read address
-	reg [31:0]							target_addr;
-	//Number of data read from one burst
-	reg [3:0]							rdata_bnum;
-	
-	
-	//Number of data read from DMA
-	reg [DATA_SIZE_LOG-1:0] 			rdata_num;
-	//number of burst happened
-	wire [DATA_SIZE_LOG-1-4:0]				rburst_num;
-	assign rburst_num = rdata_num[DATA_SIZE_LOG-1:5];
-	
-	
-	
-	
-	////////////////////////
 	//Read address channel
-	////////////////////////
+	reg	[DATA_SIZE_LOG-1:0]			rdata_count;
+	reg 							rdata_ch_active;
+
+	//Write address channel
+	reg	[DATA_SIZE_LOG-1:0]			wdata_count;
+	reg 							wdata_ch_active;
+
+	//Write response channel
+	reg [1:0]						bresp;
+
 	
-	//arvalid signal control
+	////////////////////////////////////
+	//Read address channel
+	////////////////////////////////////
+	
+	//Set read address
+	always @(posedge m_axi_acp_aclk) begin
+		if( (rdata_count == 0) || read_active )
+			m_axi_acp_araddr <= (read_active) ? read_addr : m_axi_acp_araddr + {rdata_count, 3'b000};
+	end
+	
+	//Valid signal control
 	always @(posedge m_axi_acp_aclk) begin
 		if(~axi_resetn) begin
 			m_axi_acp_arvalid <= 0;
 		end
 		else begin
-			if(rdata_bnum == 0 && ~m_axi_acp_arvalid && (rdata_num < DATA_SIZE)) begin
-				target_addr <= target_addr + rburst_num*8;
-				m_axi_acp_arvalid <= 1;
-			end
-			else if(m_axi_acp_arvalid && m_axi_acp_arready) begin
-				m_axi_acp_arvalid <= 0;
-			end
-		end
-	end
-	
-	
-	
-	////////////////////////
-	//Read data channel
-	////////////////////////
-	
-	//Receive data & count data
-	always @(posedge m_axi_acp_aclk) begin
-		if(~axi_resetn) begin
-			rdata_bnum <= 0;
-			rdata_num <= 0;
-		end
-		else begin
-			if(m_axi_acp_rready && m_axi_acp_rvalid) begin
-				//DATA_BUFFER <= m_axi_acp_rdata;
-				rdata_bnum <= rdata_bnum + 1;
-				rdata_num <= rdata_num + 1;
-				
-			end
-			
+			m_axi_acp_arvalid <= ~rdata_ch_active && (rdata_count == 0) || read_active;
 		end
 		
 	end
 
+	//Read data channel activate control
+	always @(posedge m_axi_acp_aclk) begin
+		if(~axi_resetn) begin
+			rdata_ch_active <= 0;
+		end
+		else begin															//??? grammar warning
+			rdata_ch_active <= (m_axi_acp_arvalid && m_axi_acp_arready) && (rdata_count[3:0] == 4'b1111) && ~(rdata_count[DATA_SIZE_LOG-1:4] == BURST_NUM );
+		end
+		
+	end
 
+	//Idle signal control
+	always @(posedge m_axi_acp_aclk) begin
+		if(~axi_resetn) begin
+			read_idle <= 0;
+		end
+		else begin
+			read_idle <= ((~read_active) && (~rdata_ch_active)) || (rdata_count[DATA_SIZE_LOG-1:4] == BURST_NUM );
+		end
+	end
+
+	//Data counter
+	always @(posedge m_axi_acp_aclk) begin
+		if(~axi_resetn) begin
+			rdata_count <= 0;
+		end
+		else begin
+			if(m_axi_acp_rready && m_axi_acp_rvalid)
+				rdata_count <= rdata_count + 1;
+		end
+	end
+	
+
+
+	////////////////////////////////////
+	//Read data channel
+	////////////////////////////////////
+	
+	assign mm2s_data = m_axi_acp_rdata;
+
+	assign m_axi_acp_rready = mm2s_ready && rdata_ch_active;
+	assign mm2s_valid = m_axi_acp_rvalid && rdata_ch_active;
+
+
+
+	////////////////////////////////////
+	//Write address channel
+	////////////////////////////////////
+	
+	//Set write address
+	always @(posedge m_axi_acp_aclk) begin
+		if( (wdata_count == 0) || write_active )
+			m_axi_acp_awaddr <= (write_active) ? write_addr : m_axi_acp_awaddr + {wdata_count, 3'b000};
+	end
+
+	//Valid signal control
+	always @(posedge m_axi_acp_aclk) begin
+		if(~axi_resetn) begin
+			m_axi_acp_awvalid <= 0;
+		end
+		else begin
+			m_axi_acp_awvalid <= ~wdata_ch_active && (wdata_count == 0) || write_active;
+		end
+		
+	end
+
+	//Write data channel activate control
+	always @(posedge m_axi_acp_aclk) begin
+		if(~axi_resetn) begin
+			wdata_ch_active <= 0;
+		end
+		else begin															//??? grammar warning
+			wdata_ch_active <= (m_axi_acp_awvalid && m_axi_acp_awready) && (wdata_count[3:0] == 4'b1111) && ~(wdata_count[DATA_SIZE_LOG-1:4] == BURST_NUM );
+		end
+		
+	end
+
+	//Idle signal control
+	always @(posedge m_axi_acp_aclk) begin
+		if(~axi_resetn) begin
+			write_idle <= 0;
+		end
+		else begin
+			write_idle <= ((~write_active) && (~wdata_ch_active)) || (wdata_count[DATA_SIZE_LOG-1:4] == BURST_NUM );
+		end
+	end
+
+	//Data counter
+	always @(posedge m_axi_acp_aclk) begin
+		if(~axi_resetn) begin
+			wdata_count <= 0;
+		end
+		else begin
+			if(m_axi_acp_wready && m_axi_acp_wvalid)
+				wdata_count <= wdata_count + 1;
+		end
+	end
+
+
+	////////////////////////////////////
+	//Write data channel
+	////////////////////////////////////
+
+	assign m_axi_acp_wdata = s2mm_data;
+
+	assign m_axi_acp_wvalid = s2mm_valid && wdata_ch_active;
+	assign s2mm_ready = m_axi_acp_wready && wdata_ch_active;
+
+
+	////////////////////////////////////
+	//Write response channel
+	////////////////////////////////////
+
+	always @(posedge m_axi_acp_aclk) begin
+		if(~axi_resetn) begin
+			m_axi_acp_bready <= 0;
+		end
+		else begin
+			m_axi_acp_bready <= m_axi_acp_wready && m_axi_acp_wvalid && m_axi_acp_wlast;
+		end
+	end
+
+	always @(posedge m_axi_acp_aclk) begin
+		if(~axi_resetn) begin
+			bresp <= 0;
+		end
+		else begin
+			if(m_axi_acp_wready && m_axi_acp_wvalid)
+				bresp <= m_axi_acp_bresp;
+		end
+	end
 
 
 endmodule
